1fa071ac52ec4fe6a02a194ab3e7836faa13e3c0
Kitessa Fikadu <kitessafikadu@gmail.com>
2025-10-30 22:57:55 +0300

    Add palindrome

--- 2-go-fundamentals/palindrome/palindrome.go
+++ 2-go-fundamentals/palindrome/palindrome.go
@@ -1,1 +1,21 @@
-// check palindrome string
+package palindrome
+
+import (
+	"regexp"
+	"strings"
+)
+
+// IsPalindrome checks if a string is a palindrome ignoring punctuation and case.
+func IsPalindrome(s string) bool {
+	s = strings.ToLower(s)
+	re := regexp.MustCompile(`[^a-z0-9]`)
+	clean := re.ReplaceAllString(s, "")
+
+	n := len(clean)
+	for i := 0; i < n/2; i++ {
+		if clean[i] != clean[n-1-i] {
+			return false
+		}
+	}
+	return true
+}
